b2bf8a1005bde38b0ee4b0853245303e42cf9da9
colonaut <colonaut@gmx.de>
2014-01-05 23:02:54 +0100

    Some debug sniffig for test results with UrlInfoBase
    This will disappear once a mockable 8singleton) chachehas been
    developped

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -1,7 +1,10 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
 using System.Web.Routing;
 using CollectionJsonExtended.Client;
 using CollectionJsonExtended.Client.Attributes;
+using CollectionJsonExtended.Core;
 using MedienKultur.Gurps.Controllers;
 using MedienKultur.Gurps.Models;
 using MedienKultur.RequireScriptResolver;
@@ -24,8 +27,17 @@ namespace MedienKultur.Gurps.App_Start
 
             routes.MapMvcAttributeRoutes(); //also maps the CollectionJsonRoutes
 
-            var attempt1DebugDictionary = RouteInfo.GetCacheForDebug();
-            var attempt2DebugDictionary = RouteInfo.Find(typeof(GameSession));
+
+            var attempt1DebugDictionary = UrlInfoBase.Find(typeof(GurpsCharacter));
+
+            var attempt2DebugDictionary = RouteInfo.FindRouteInfos(typeof(GurpsCharacter));
+
+            var attempt3DebugDictionary = RouteInfo.FindRouteInfos(typeof(GameSession));
+
+
+
+            var attempt4DebugDictionary = UrlInfoBase.Find(typeof(Article)) as IEnumerable<RouteInfo>;
+            var attempt5DebugDictionary = UrlInfoBase.Find(typeof(Category)) as IEnumerable<RouteInfo>;
             
             //CollectionJson
             routes.MapRoute(
